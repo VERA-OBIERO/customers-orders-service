3d871c5869b34cbee033bfd2057c0942d4058c4c
VERA-OBIERO <vera.obiero@gmail.com>
2024-02-19 20:12:59 +0300

    define database models

---
+++ models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(255), nullable=False)
+    lastname = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(255), unique=True, nullable=False)
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(255), unique=True, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    customer_code = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    
+    customer = db.relationship('Customer', backref=db.backref('orders', lazy=True))
