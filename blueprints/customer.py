22b9839b5a4fe98b472d2803ab8d92bcc927ef29
VERA-OBIERO <vera.obiero@gmail.com>
2024-02-20 18:53:58 +0300

    POST new customer

--- blueprints/customer.py
+++ blueprints/customer.py
@@ -6,6 +6,12 @@ from serializers import CustomerSchema
 customer_bp = Blueprint('customer', __name__)
 api = Api(customer_bp)
 
+post_args = reqparse.RequestParser()
+post_args.add_argument('firstname', type=str, required=True)
+post_args.add_argument('lastname', type=str, required=True)
+post_args.add_argument('email', type=str, required=True)
+post_args.add_argument('phone', type=str, required=True)
+
 patch_args = reqparse.RequestParser()
 patch_args.add_argument('firstname', type=str)
 patch_args.add_argument('lastname', type=str)
@@ -19,6 +25,14 @@ class Customers(Resource):
     def get(self):
         customers = Customer.query.all()
         return customer_schema.dump(customers, many=True)
+    
+    def post(self):
+        data = post_args.parse_args()
+
+        new_customer = Customer(firstname=data['firstname'], lastname=data['lastname'], email=data['email'], phone=data['phone'])
+        db.session.add(new_customer)
+        db.session.commit()
+        return customer_schema.dump(new_customer)
 
 class CustomerById(Resource):
    
