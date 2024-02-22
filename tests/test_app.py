47186c14b62717c2f528b61c7ca1b445d6b85eb4
VERA-OBIERO <vera.obiero@gmail.com>
2024-02-22 19:46:26 +0300

    write unit tests

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+import unittest
+from app import app, db
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        with app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
